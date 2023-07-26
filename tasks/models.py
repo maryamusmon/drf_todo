c08d925e286c099898f82fe43183fd58d88a7c95
ozoda <ozodas531@gmail.com>
2023-07-26 12:51:09 +0500

    update models

--- tasks/models.py
+++ tasks/models.py
@@ -11,14 +11,14 @@ class Board(Model):
 
 class Column(Model):
     name = CharField(max_length=200)
-    board = ForeignKey(Board, CASCADE)
+    board = ForeignKey(Board, CASCADE, related_name='columns')
 
 
 class Tasks(Model):
     title = CharField(max_length=200)
     description = TextField(blank=True, null=True)
 
-    status = ForeignKey(Column, CASCADE)
+    status = ForeignKey(Column, CASCADE, related_name='tasks')
 
     class Meta:
         db_table = 'tasks'
@@ -32,4 +32,4 @@ class Tasks(Model):
 class Subtasks(Model):
     name = CharField(max_length=200)
     is_completed = BooleanField(default=False)
-    task = ForeignKey(Tasks, CASCADE)
+    task = ForeignKey(Tasks, CASCADE, related_name='subtasks')
--- tasks/serializer.py
+++ tasks/serializer.py
@@ -1,6 +1,21 @@
-from rest_framework.serializers import  ModelSerializer
+from rest_framework.relations import RelatedField
+from rest_framework.serializers import ModelSerializer
 
-from tasks.models import Column, Board, Tasks, Subtasks
+from tasks.models import Column, Board, Tasks
+
+
+class SubTaskSerializer(RelatedField):
+
+    def to_representation(self, value):
+        return {'id': value.id, 'title': value.name}
+
+
+class TasksSerializer(ModelSerializer):
+    subtasks = SubTaskSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Tasks
+        fields = ('id', 'title', 'subtasks')
 
 
 class BoardModelSerializer(ModelSerializer):
@@ -10,8 +25,27 @@ class BoardModelSerializer(ModelSerializer):
 
     def to_representation(self, instance: Board):
         rep = super().to_representation(instance)
-        if instance.column_set.all():
-            rep['columns'] = instance.column_set.values('id', 'name', 'board')
+        if instance.columns.all():
+            result = []
+            for column in instance.columns.values('id', 'name', 'board'):
+                tasks = []
+                for task in instance.columns.values('tasks__id', 'tasks__title', 'tasks__description', 'tasks__status'):
+                    if task.get('tasks__status') == column.get('id'):
+                        subtasks = []
+                        for subtask in instance.columns.values('tasks__subtasks', 'tasks__subtasks__name',
+                                                               'tasks__subtasks__is_completed', 'tasks'):
+                            if subtask.get('tasks') == task.get('tasks__id'):
+                                subtasks.append(subtask)
+                            else:
+                                continue
+                        task['subtasks'] = subtasks
+                        tasks.append(task)
+                    else:
+                        continue
+                # column['tasks'] = TasksSerializer(column, many=True, read_only=True)
+                column['tasks'] = tasks
+                result.append(column)
+            rep['columns'] = result
             return rep
         return rep
 
@@ -23,7 +57,7 @@ class ColumnModelSerializer(ModelSerializer):
 
     def to_representation(self, instance: Column):
         result = super().to_representation(instance)
-        if instance.tasks_set.all():
+        if instance.tasks.all():
             result['tasks'] = instance.tasks_set.values('id', 'title', 'description', 'status')
             return result
         return result
@@ -36,7 +70,7 @@ class TaskModelSerializer(ModelSerializer):
 
     def to_representation(self, instance: Tasks):
         result = super().to_representation(instance)
-        if instance.subtasks_set.all():
+        if instance.subtasks.all():
             result['subtasks'] = instance.subtasks_set.values('id', 'name', 'is_completed')
             return result
         return result
