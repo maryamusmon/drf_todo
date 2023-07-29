e591a439fb4d263d7ace494edf31c2b519716463
diordev <diordev@icloud.com>
2023-07-29 12:23:04 +0500

    change setting

--- root/urls.py
+++ root/urls.py
@@ -2,16 +2,16 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from root.settings import STATIC_URL, STATIC_ROOT
+from root.settings import STATIC_URL, STATIC_ROOT, MEDIA_ROOT, MEDIA_URL
 from root.swagger import swagger_urls
 from users.views import UserTokenObtainPairView, UserTokenRefreshView, UserTokenVerifyView
+urlpatterns = []
+urlpatterns += swagger_urls +[
+                   path('admin/', admin.site.urls),
+                   path('api/v1/', include('tasks.urls')),
+                   path('users/', include('users.urls')),
+                   path('token/create/', UserTokenObtainPairView.as_view(), name='token_create'),
+                   path('token/refresh/', UserTokenRefreshView.as_view(), name='token_refresh'),
+                   path('token/verify/', UserTokenVerifyView.as_view(), name='token_verify'),
 
-urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  path('api/v1/', include('tasks.urls')),
-                  path('users/', include('users.urls')),
-                  path('token/create/', UserTokenObtainPairView.as_view(), name='token_create'),
-                  path('token/refresh/', UserTokenRefreshView.as_view(), name='token_refresh'),
-                  path('token/verify/', UserTokenVerifyView.as_view(), name='token_verify'),
-
-              ] + swagger_urls + static(STATIC_URL, static_root=STATIC_ROOT)
+               ] + static(STATIC_URL, document_root=STATIC_ROOT) + static(MEDIA_URL, document_root=MEDIA_ROOT)
