bf589989c60b8c7b17c2d53c87d165ee2c41290b
mariyamusmon <ozodas531@gmail.com>
2023-08-07 13:34:16 +0500

    update urls.py

--- users/urls.py
+++ users/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     path('activate/', ActivationUserGenericAPIView.as_view(), name='activated_account'),
     path('reset-password/', PasswordResetGenericAPIView.as_view(), name='reset_password'),
     path('reset-password-confirm/', PasswordResetConfirmUpdateAPIView.as_view(), name='reset_password_confirm'),
-    path('me/', UserRetrieveAPIView.as_view(), name='me'),
+    path('me/', UserRetrieveAPIView.as_view(), name='me')
 ]
