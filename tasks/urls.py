f20473b78ee464c163b357cf37bbe9e5ccf83142
mariyamusmon <ozodas531@gmail.com>
2023-08-15 12:38:13 +0500

    add task in pytest

--- tasks/urls.py
+++ tasks/urls.py
@@ -11,17 +11,26 @@ from tasks.views import (
     TaskCreateAPIView,
 )
 
-router = DefaultRouter()
-router.register(r'boards', BoardListCreateAPIView, basename='board')
-router.register(r'task', TaskCreateAPIView, basename='task')
+# router = DefaultRouter()
+# router.register(r'boards', BoardListCreateAPIView, basename='board')
+# router.register(r'task', TaskCreateAPIView, basename='task')
 
+board_list = BoardListCreateAPIView.as_view({
+    'get': 'list',
+    'post': 'create'
+
+})
+
+task_create = TaskCreateAPIView.as_view({
+    'post': 'create',
+})
 urlpatterns = [
-    path('', include(router.urls)),
+    # path('', include(router.urls)),
     path('board/<int:pk>', BoardRetrieveUpdateDestroyAPIView.as_view()),
     path('task/<int:pk>', TaskRetrieveUpdateDestroyAPIView.as_view()),
     path('column/create', ColumnCreateAPIView.as_view()),
     path('column/<int:pk>', ColumnRetrieveUpdateDestroyAPIView.as_view()),
     path('task/list/<int:pk>', TaskList.as_view()),
-    # path('board_list', BoardListCreateAPIView.as_view(), name='board'),
-    # path('task_create', TaskCreateAPIView.as_view(), name='task')
+    path('board_list', board_list, name='board'),
+    path('task_create', task_create, name='task')
 ]
